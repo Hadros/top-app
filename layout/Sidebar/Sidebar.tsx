2a01d8dcb3585746b885908646bc82d9597f3d9a
Slava Dergachyov <mr.hadros@gmail.com>
2023-04-21 12:18:26 +0300

    ADDED: routes.

--- layout/Sidebar/Sidebar.tsx
+++ layout/Sidebar/Sidebar.tsx
@@ -1,12 +1,16 @@
-import styles from './Sidebar.module.css';
 import {SidebarProps} from "./Sidebar.props";
 import { Menu } from '../Menu/Menu'
+import Logo from '../logo.svg'
+import cn from 'classnames';
+import styles from './Sidebar.module.css';
 
-export const Sidebar = ({ ...props }: SidebarProps): JSX.Element => {
+export const Sidebar = ({ className, ...props }: SidebarProps): JSX.Element => {
 
   return (
     <>
-      <div {...props}>
+      <div {...props} className={cn(className, styles.sidebar)}>
+        <Logo className={styles.logo}/>
+        <div>поиск</div>
         <Menu />
       </div>
     </>
